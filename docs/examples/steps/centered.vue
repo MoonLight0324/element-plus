db3076bbe853c5373626727c0741e5aa66860c39
MoonLight0324 <358069434@qq.com>
2025-03-07 16:09:46 +0800

    Update centered.vue

--- docs/examples/steps/centered.vue
+++ docs/examples/steps/centered.vue
@@ -1,7 +1,8 @@
 <template>
   <el-steps style="max-width: 600px" :active="2" align-center>
-    <el-step title="Step 1" description="Some description" />
-    <el-step title="Step 2" description="Some description" />
-    <el-step title="Step 3" description="Some description" />
+    <el-step title="Step 1" description="选择模板" />
+    <el-step title="Step 2" description="基础信息填写" />
+    <el-step title="Step 3" description="编写步骤" />
+    <el-step title="Step 4" description="导出MOP" />
   </el-steps>
 </template>
